84074839b3fba629b588ff0a3c61080d38435983
mohitsharmadev <ms2130075@gmail.com>
2023-12-15 15:46:14 +0530

    JavaScript

---
+++ Javascript/Data-Types.js
@@ -0,0 +1,48 @@
+/* 8 DataType
+1.string 
+2.number
+3.boolean
+4.bigint
+5.object
+6.undefined
+7.null
+8.symbol
+
+*/
+//string
+let a = "mohit";
+let b = "sharma";
+
+//number
+let c = 6;
+let d = 2.56;
+
+//boolean
+let e = true;
+let f = false;
+
+//bigint
+let g = BigInt("1,2,3,4,5,6,7,8,9,4,5,6,1,2.3,4,5,6,6,63,8,65,2,6");
+
+//Undefined
+let un;
+let def;
+
+//Empty value
+let k = "";
+
+//null
+let h = null;
+let j = null;
+
+//object
+let object = {
+  firstName: "mohit",
+  lastName: "sharma",
+};
+
+//symbol
+let ab = Symbol("string ");
+
+//Array
+let array = [1, 2, 3, 5, "array"];
