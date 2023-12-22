f6a66196c1d21dd657253b00e306bd17abf6a11f
mohitsharmadev <ms2130075@gmail.com>
2023-12-22 23:51:51 +0530

    Javascript-operators

---
+++ Javascript/operators.js
@@ -0,0 +1,19 @@
+//Arithmetic operator
+
+let a = 4 + 5;
+//Substraction
+let b = 4 - 5;
+//Multiplication
+let c = 4 * 8;
+//Division
+let d = 2 / 9;
+//Exponentiation
+let e = 9 ** 8;
+//Module
+let f = 9 % 5;
+//Increment
+let g = a++;
+//Decrement
+let h = b++;
+
+//Comparison operator
