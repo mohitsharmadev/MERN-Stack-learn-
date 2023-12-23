8f20be392f1deb0d2065aa5f702f517b5bcd7a9e
mohitsharmadev <ms2130075@gmail.com>
2023-12-23 23:05:53 +0530

    Add operators

--- Javascript/operators.js
+++ Javascript/operators.js
@@ -16,4 +16,54 @@ let g = a++;
 //Decrement
 let h = b++;
 
+//Assignment operator
+let i, j, k, l, m, n;
+i += 5;
+j -= 6;
+k *= 7;
+l /= 9;
+m %= 8;
+n ** 9;
+
 //Comparison operator
+//equal to
+let o, p, q, r, s, t, w, x, y;
+//equal to
+o == 4;
+//equal to and equal type
+p === 5;
+//not equal
+q != 7;
+//not equal and not equal type
+r !== 8;
+//greater than
+s > 8;
+//lesser than
+t < 9;
+//greater than equal to
+w >= 9;
+//lesser than equal to
+x <= 9;
+// ? ternary operator
+
+//Logical operators
+//&& logical and
+// || Logical or
+// ! Logical not
+
+//Bitwise operator
+let A, B, C, D, E, F;
+//AND
+A = 5 & 6;
+//OR
+B = 8 | 9;
+//Not
+C = ~9;
+//Xor
+E = 8 ^ 9;
+//right shift
+D = 9 >> 8;
+//left shift
+F = 7 << 0;
+//Unsigned right shift
+y = 8 >>> 0;
