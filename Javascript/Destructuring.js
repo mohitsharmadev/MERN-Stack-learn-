eefeae996e6953c92a635794c3da18380d075934
mohitsharmadev <ms2130075@gmail.com>
2024-01-01 00:31:14 +0530

    changes

--- Javascript/Destructuring.js
+++ Javascript/Destructuring.js
@@ -1,14 +1,10 @@
-let array = [1, 2, 3, 4];
+let array = [1, 2, 3, 4, 9, 6];
 //this is destructuring
-let [a, b, c, d] = array;
+let [a, b, c, d, ...rest] = array;
 //Not need doing this
 // let a = array[0];
 // let b=array[1];
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
-
+console.log(a, b, c, d, rest);
 //destructure objects
 let { o, m } = { o: 1, m: 5 };
 console.log(o, m);
